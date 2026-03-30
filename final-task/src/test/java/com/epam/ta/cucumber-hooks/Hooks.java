4b57d62f931d9fcb4d5010a96f40623acc81a891
saba-beridze-a1qa <s.beridze@a1qa.com>
2026-03-30 22:16:05 +0400

    jenkins test 5

--- final-task/src/test/java/com/epam/ta/cucumber-hooks/Hooks.java
+++ final-task/src/test/java/com/epam/ta/cucumber-hooks/Hooks.java
@@ -3,11 +3,15 @@ package com.epam.ta.steps;
 import com.epam.ta.driver.DriverSingleton;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.qameta.allure.Allure;
 
 public class Hooks {
 
     @Before
     public void setUp() {
+        String browser = System.getProperty("browser", "unknown");
+        Allure.label("browser", browser);
+
         DriverSingleton.getDriver();
     }
 
