753202e8333b7482988ab8c1689e9c8e078470da
saba-beridze-a1qa <s.beridze@a1qa.com>
2026-03-30 20:39:34 +0400

    removed testng and now it is junit 5 based.

--- final-task/src/test/java/com/epam/ta/runner/CucumberRunner.java
+++ final-task/src/test/java/com/epam/ta/runner/CucumberRunner.java
@@ -1,15 +1,18 @@
-package com.epam.ta.runner;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.SelectPackages;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "com.epam.ta.steps",
-        plugin = {
-                "pretty",
-                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
-        }
-)
-public class CucumberRunner extends AbstractTestNGCucumberTests {
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@SelectPackages("com.epam.ta.runner")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.epam.ta.steps")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value =
+        "pretty, io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
+public class CucumberRunner {
 }
